0e17a3e06a0337ad5b110a487ef27d10714872f4
adil <adil@kiebot.com>
2022-01-03 00:52:23 +0530

    Add merge sort

---
+++ merge_sort.js
@@ -0,0 +1,39 @@
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr
+    }
+
+    let arrMid = Math.floor(arr.length / 2)
+    console.log('mid', arrMid)
+    let left = mergeSort(arr.slice(0, arrMid))
+    let right = mergeSort(arr.slice(arrMid))
+console.log('left', left)
+console.log('right', right)
+
+    return merge(left, right)
+}
+
+function merge(leftArr, rightArr) {
+    let rightIndex = 0;
+    let leftIndex = 0;
+    let res = []
+
+    while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+        let leftEle = leftArr[leftIndex]
+        let rightEle = rightArr[rightIndex]
+
+        if (leftEle < rightEle) {
+            leftIndex++
+            res.push(leftEle)
+        } else {
+            rightIndex++
+            res.push(rightEle)
+        }
+    }
+
+    return [...res, ...leftArr.slice(leftIndex), ...rightArr.slice(rightIndex)];
+}
+
+res = mergeSort([4,3,5,9,1])
+
+console.log(res)
