b3cd9a993dcff121b60cbee12fca293a5d102123
adil <adilismail62@gmail.com>
2022-10-24 02:23:28 +0530

    Bug fix

--- LEETCODE_search_insert_position.rb
+++ LEETCODE_search_insert_position.rb
@@ -6,28 +6,53 @@
 # Output: 1
 # Input: nums = [1,3,5,6], target = 7
 # Output: 4
-
-def find_mid(length)
-    mid = (length-1) / 2
-    mid.floor
-end
-
 # @param {Integer[]} nums
 # @param {Integer} target
 # @return {Integer}
+
+def find_mid(start_idx, end_idx)
+    mid = (start_idx + (end_idx - start_idx).abs / 2)
+    mid.floor
+end
+
 def search_insert(nums, target)
-    mid = find_mid(nums.length)
+    mid = find_mid(0, nums.length)
+
+    nums.each_with_index do |num, index|
+        if num == target
+            return index
+        end
+    end
     
+    if nums.length == 1
+        if target > nums[0]
+            return 1
+        else
+            return 0
+        end
+    end
+
+    if nums.length == 2
+        if target > nums[0]
+            if target > nums[1]
+                return 2
+            end
+            return 1
+        else 
+            return 0
+        end
+    end
+
     while mid !=0 do
         direction = nums[mid] < target ? 'right' : 'left'
         start_idx = direction == 'right' ? mid + 1 : 0
-        end_idx = direction == 'right' ? nums.length-1 : mid
+        end_idx = direction == 'right' ? nums.length-1 : mid -1
 
         if nums.length == 1
             return 0
         end
 
-        if start_idx == end_idx - 1
+        if start_idx == end_idx - 1 || start_idx == end_idx
             if target > nums[start_idx]
                 if target > nums[end_idx]
                     return end_idx + 1
@@ -38,11 +63,6 @@ def search_insert(nums, target)
             end
         end
 
-        mid = find_mid((end_idx - start_idx) + 1)
-        puts "MID IS #{mid}"
+        mid = find_mid(start_idx, end_idx)
     end
 end
-
-puts search_insert([1,3,5,6], 2)
-
- 
